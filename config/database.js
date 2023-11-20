a7c7a08cd02179c626d24d185da9f1eed3c1389e
cristinavoda <143340472+cristinavoda@users.noreply.github.com>
2023-11-20 09:17:55 +0100

    Update database.js

--- config/database.js
+++ config/database.js
@@ -4,8 +4,7 @@ const mysql = require("mysql2")
 const DB_HOST = process.env.DB_HOST
 const DB_USER = process.env.DB_USER
 const DB_PASS = process.env.DB_PASS
-const DB_NAME = process.env.DB_NAME
-
+const DB_NAME = programacion
 
 const pool = mysql.createPool({
     host:3306,
