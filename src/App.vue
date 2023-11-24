24ce68ed04544883bf9b6a8a55e516398bcd7502
cristinavoda <143340472+cristinavoda@users.noreply.github.com>
2023-11-24 09:34:13 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,138 +1,136 @@
 <template>
-  <h1 class="text-3xl font-bold underline text-blue-800">Hello world!</h1>
-  <div class="flex items-center gap-10">
-    <!-- form : post -->
-    <form class="flex flex-col">
-      <label for="name">Name</label>
-      <input name="name" v-model="name" />
-      <label for="price">Price</label>
-      <input name="price" v-model="price" />
-      <label for="description">Description</label>
-      <input name="description" v-model="description" />
-      <label for="room">Room</label>
-      <input name="room" v-model="room" />
-      <button class="mt-4" @click.prevent="postProduct">Add product</button>
-    </form>
+  <div>
+    <h1 class="text-3xl font-bold underline text-green-800">Flower Shop</h1>
+    <div class="flex items-center gap-10">
+      <!-- Formulario para agregar flores -->
+      <form class="flex flex-col">
+        <label for="name">Name</label>
+        <input name="name" v-model="name" />
 
-    <!-- ------- -->
-    <ul class="flex flex-col gap-5">
-      <li
-        class="flex items-center gap-5 justify-between"
-        v-for="product in products"
-        :key="product.id"
-      >
-        <p @click.prevent="getProduct(product.id)">{{ product.name }}</p>
-        <div class="flex gap-4">
-          <button
-            class="bg-blue-500 hover:bg-blue-400 text-xs"
-            @click.prevent="updateProduct(product.id)"
-          >
-            update
-          </button>
-          <button
-            class="bg-red-500 hover:bg-red-400 text-xs"
-            @click.prevent="deleteProduct(product.id)"
-          >
-            X
-          </button>
-        </div>
-      </li>
-    </ul>
-    <div class="flex flex-col bg-white rounded-lg text-black px-4 py-4" v-if="product">
-      <h2>{{ product.name }}</h2>
-      <p>{{ product.price }}</p>
-      <p>{{ product.description }}</p>
+        <label for="bloom">Bloom</label>
+        <input name="bloom" v-model="bloom" />
+
+        <label for="lifetime">Lifetime</label>
+        <input name="lifetime" v-model="lifetime" />
+
+        <button class="mt-4" @click.prevent="postFlower">Add Flower</button>
+      </form>
+
+      <!-- Lista de flores -->
+      <ul class="flex flex-col gap-5">
+        <li
+          class="flex items-center gap-5 justify-between"
+          v-for="flower in flowers"
+          :key="flower.id"
+        >
+          <p @click.prevent="getFlower(flower.id)">{{ flower.name }}</p>
+          <div class="flex gap-4">
+            <button
+              class="bg-green-500 hover:bg-green-400 text-xs"
+              @click.prevent="updateFlower(flower.id)"
+            >
+              Update
+            </button>
+            <button
+              class="bg-red-500 hover:bg-red-400 text-xs"
+              @click.prevent="deleteFlower(flower.id)"
+            >
+              X
+            </button>
+          </div>
+        </li>
+      </ul>
+
+      <!-- Detalles de la flor seleccionada -->
+      <div class="flex flex-col bg-white rounded-lg text-black px-4 py-4" v-if="flower">
+        <h2>{{ flower.name }}</h2>
+        <p>Bloom: {{ flower.bloom }}</p>
+        <p>Lifetime: {{ flower.lifetime }}</p>
+      </div>
+
+      <button @click.prevent="getFlowers">Get Flowers</button>
     </div>
-    <button @click.prevent="getProducts">get products</button>
   </div>
 </template>
 
 <script>
-export default {
+export default :{
   name: "App",
   data() {
     return {
-      products: [],
-      product: "",
+      flowers: [],
+      flower: "",
       name: "",
-      price: "",
-      description: "",
-      room: "",
+      bloom: "",
+      lifetime: "",
     };
-  },
+  }},
   methods: {
-    async getProducts() {
-      const response = await fetch("http://localhost:8888/api/home-products");
+    async getFlowers() {
+      const response = await fetch("http://localhost:8888/api/flowers");
       const data = await response.json();
-      return (this.products = data.data);
+      this.flowers = data.data;
     },
-    async getProduct(id) {
-      const response = await fetch(
-        `http://localhost:8888/api/home-products/${id}`
-      );
+    async getFlower(id) {
+      const response = await fetch(`http://localhost:8888/api/flowers/${id}`);
       const data = await response.json();
-      return (this.product = data.data[0]);
+      this.flower = data.data[0];
     },
-    async postProduct() {
-      const newProduct = {
+    async postFlower() {
+      const newFlower = {
         name: this.name,
-        price: this.price,
-        description: this.description,
-        room: this.room,
-      };
-      this.name = "",
-      this.price = "",
-      this.description = "",
-      this.room = ""
-      
-      const response = await fetch("http://localhost:8888/api/home-products", {
+        bloom: this.bloom,
+        lifetime: this.lifetime,
+
+      this:name = "",
+      this:bloom = "",
+      this:lifetime = "",
+
+      const :response = await fetch("http://localhost:8888/api/flowers", {
         method: "POST",
         headers: { "Content-type": "application/json" },
-        body: JSON.stringify(newProduct),
+        body: JSON.stringify(newFlower),
+      }),
+
+      const : data = await response.json(),
+      this:getFlowers(),
+    },
+    async ,updateFlower ()=>{
+      const updateFlower ={
+        name:any,
+        bloom : any,
+        lifetime:any,
+      },
+      
+
+      this. name = "",
+      this.bloom = "",
+      this.lifetime = "",
+     try :{
+
+      const response = await fetch(`http://localhost:5500/api/flowers/${id}`, {
+        method: "PUT",
+        headers: { "Content-type": "application/json" },
+        body: JSON.stringify(updatedFlower),
       });
-      const data = await response.json();
-      return this.getProducts();
-    },
-    async updateProduct(id) {
-      const newProduct = {
-        name: this.name,
-        price: this.price,
-        description: this.description,
-        room: this.room,
-      };
-      this.name = "",
-      this.price = "",
-      this.description = "",
-      this.room = ""
-      const response = await fetch(
-        `http://localhost:8888/api/home-products/${id}`,
-        {
-          method: "PUT",
-          headers: { "Content-type": "application/json" },
-          body: JSON.stringify(newProduct),
-        }
-      );
-      const data = await response.json();
-      return this.getProducts();
-    },
 
-    async deleteProduct(id) {
-      try{
-        const response = await fetch(
-        `http://localhost:8888/api/home-products/${id}`,
-        {
+      const: data = await response.json();
+      this.getFlowers();
+    },
+    const async : deleteFlower(id:any) =>
+      try {
+        const response = await fetch(`http://localhost:8888/api/flowers/${id}`, {
           method: "DELETE",
-        }
-      );
-      const data = await response.json();
-      return this.getProducts();
+        });
 
-      }catch(err){
-        console.log(err, "deletProduct failed")
+        const :dataFlower = await response.json();
+        this.getFlowers();
+      } catch (err) {
+        console.log("deleteFlower failed", err);
       }
     },
   },
-};
+},
 </script>
 
 <style></style>
